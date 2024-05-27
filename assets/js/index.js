15b84adfbcbc3f82e6348730931d98e6c9b6a1d9
Michela Ballone <michelaballone762@gmail.com>
2024-05-27 11:12:38 +0200

    ritorno

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  const x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
